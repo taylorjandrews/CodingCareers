a39cb2c89e173531b96d22d39b85e79319141d39
Pezhin <p@oath.pl>
2016-04-04 12:45:50 -0600

    AboutPageBody 1.0

--- WebApp/src/codingcareers/webapp/client/PageComponents/AboutPageBody.java
+++ WebApp/src/codingcareers/webapp/client/PageComponents/AboutPageBody.java
@@ -1,8 +1,26 @@
 package codingcareers.webapp.client.PageComponents;
 
+import com.google.gwt.user.client.ui.DockPanel;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.Label;
+
+import codingcareers.webapp.client.Constants;
+
 public class AboutPageBody extends PageBody{
 
-
+	public AboutPageBody(){
+		Label welcomeMessage = new Label();
+		welcomeMessage.setText("Welcome to "+ Constants.TITLE);
+		add(welcomeMessage, DockPanel.NORTH);
+		
+		Label textMessage = new Label();
+		textMessage.setText("There should be some welcome messages in the Constant class files");
+		add(textMessage, DockPanel.CENTER);
+		
+		Anchor tryItNow = new Anchor("Try it Now!","http://domain.com/index.tml");
+		
+		add(tryItNow, DockPanel.SOUTH);
+	}
 	@Override
 	public void attachHandlers() {
 		//STUB
