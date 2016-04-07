a7421c5a33271bc3c986f9f55fc5ada554272ad7
Alan Moy <alan.moy@colorado.edu>
2016-04-06 23:14:36 -0600

    Implement Python interpreter/tests in TaskPageBody

--- WebApp/src/codingcareers/webapp/client/CodingCareers.java
+++ WebApp/src/codingcareers/webapp/client/CodingCareers.java
@@ -3,7 +3,10 @@ package codingcareers.webapp.client;
 import codingcareers.webapp.client.PageComponents.*;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
+//import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
+import com.google.gwt.core.client.ScriptInjector;
+import com.google.gwt.core.client.Callback;
+import com.google.gwt.user.client.Window;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -35,13 +38,31 @@ public class CodingCareers implements EntryPoint {
 //      PageComposite assembled = new PageComposite(header, content, footer);
 //      assembled.load();
 
-      PageBody content;
-      try {
-          content = bodyFactory.buildPageBody(Constants.LOGIN_PAGE);
-      } catch(InvalidPageException e) {
-          System.out.println("Exception thrown:" + e);
-          return;
+    ScriptInjector.fromUrl(GWT.getModuleBaseForStaticFiles() +
+        "skulpt.min.js").setCallback(new Callback() {
+      public void onFailure(Object reason) {
+        Window.alert("Script load failed");
       }
-      PageCompositeFlyweightFactory.getInstance().buildPage(content).load();
+      public void onSuccess(Object result) {
+      }
+    }).inject();
+
+    ScriptInjector.fromUrl(GWT.getModuleBaseForStaticFiles() +
+        "skulpt-stdlib.js").setCallback(new Callback() {
+      public void onFailure(Object reason) {
+        Window.alert("Script load failed");
+      }
+      public void onSuccess(Object result) {
+      }
+    }).inject();
+
+    PageBody content;
+    try {
+      content = bodyFactory.buildPageBody(Constants.TASK_PAGE);
+    } catch(InvalidPageException e) {
+      System.out.println("Exception thrown:" + e);
+      return;
+    }
+    PageCompositeFlyweightFactory.getInstance().buildPage(content).load();
   }
 }
