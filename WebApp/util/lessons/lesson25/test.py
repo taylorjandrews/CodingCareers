653a0e842db24ac0f7b42511d58a2ceca7d345ba
Alan Moy <alan.moy@colorado.edu>
2016-04-15 23:46:58 -0600

    Setup scripts to construct DB with task info

---
+++ WebApp/util/lessons/lesson25/test.py
@@ -0,0 +1,11 @@
+# f(n) calculates nth fibonacci number
+
+def CodingCareers__EvaluateLevel():
+    cc = CodingCareers__()
+    cc.expect(f(0) == 0)
+    cc.expect(f(5) == 5)
+    cc.expect(f(10) == 55)
+    cc.report()
+
+CodingCareers__EvaluateLevel()
+
