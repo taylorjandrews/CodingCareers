684cc67f9cbe9ffbd8e80b4daac8cee09b4a11c6
Lauren Mitchell <lauren.a.mitchell@colorado.edu>
2016-04-21 18:22:31 -0600

    creating test cases for intro level and level 1

--- WebApp/util/lessons/lesson25/test.py
+++ WebApp/util/lessons/lesson25/test.py
@@ -1,11 +1,36 @@
 # f(n) calculates nth fibonacci number
 
-def CodingCareers__EvaluateLevel():
-    cc = CodingCareers__()
-    cc.expect(f(0) == 0)
-    cc.expect(f(5) == 5)
-    cc.expect(f(10) == 55)
-    cc.report()
+import unittest
+    #def CodingCareers__EvaluateLevel():
+    #cc = CodingCareers__()
+    #cc.expect(f(0) == 0)
+    #cc.expect(f(5) == 5)
+    #cc.expect(f(10) == 55)
+#cc.report()
 
-CodingCareers__EvaluateLevel()
+#CodingCareers__EvaluateLevel()
 
+
+#hello world
+#variables
+#input
+
+def test_introlevel():
+    from level1 import introLevel, level1
+    import sys
+    from StringIO import StringIO
+    
+    out = StringIO()
+    sys.stdout = out
+    introLevel()
+    output = out.getvalue().strip()
+    assert output == 'hello world!'
+
+
+    out = StringIO()
+    sys.stdout = out
+    level1()
+    output = out.getvalue().strip()
+    assert output == 'april 2016' or 'April 2016'
+    
+test_introlevel()
