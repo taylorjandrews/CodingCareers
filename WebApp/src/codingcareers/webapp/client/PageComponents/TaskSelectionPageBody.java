4a051c81a0571e68776e4af585ca46fdd8fb26d0
Pezhin <p@oath.pl>
2016-04-24 19:12:52 -0600

    css for TaskSelectionPage

--- WebApp/src/codingcareers/webapp/client/PageComponents/TaskSelectionPageBody.java
+++ WebApp/src/codingcareers/webapp/client/PageComponents/TaskSelectionPageBody.java
@@ -16,30 +16,55 @@ import java.util.ArrayList;
 public class TaskSelectionPageBody extends PageBody{
 
 	public TaskSelectionPageBody() {
+        this.addStyleName("pp");
 	    VerticalPanel vp = new VerticalPanel();
 		Label title1 = new Label("Lesson 1: Help through Code");
-		Label title2 = new Label("Lesson 2: NASA"); 
-		Label title3 = new Label("Lesson 3: The FBI"); 
-		Label title4 = new Label("Lesson 4: iOS-Developer"); 
+		Label title2 = new Label("Lesson 2: NASA");
+		Label title3 = new Label("Lesson 3: The FBI");
+		Label title4 = new Label("Lesson 4: iOS-Developer");
+
 
 	    FlowPanel fp = new FlowPanel();
-	    Image image1 = genImage("http://www.tutorialspoint.com/images/gwt-mini.png"); 
-	    Image image2 = genImage("http://www.tutorialspoint.com/images/gwt-mini.png"); 
-	    Image image3 = genImage("http://www.tutorialspoint.com/images/gwt-mini.png"); 
-	    Image image4 = genImage("http://www.tutorialspoint.com/images/gwt-mini.png"); 
+	    FlowPanel fp2 = new FlowPanel();
+	    FlowPanel fp3 = new FlowPanel();
+	    FlowPanel fp4 = new FlowPanel();
 
-	    fp.add(image1); 
-	    fp.add(image2); 
-	    fp.add(image4); 
-        fp.insert(image3, 3); 
+
+	    Image image1 = genImage("https://pbs.twimg.com/profile_images/454613053103828993/nxzP0n4r.jpeg");
+	    Image image2 = genImage("https://pbs.twimg.com/profile_images/454613053103828993/nxzP0n4r.jpeg");
+	    Image image3 = genImage("https://pbs.twimg.com/profile_images/454613053103828993/nxzP0n4r.jpeg");
+	    Image image4 = genImage("https://pbs.twimg.com/profile_images/454613053103828993/nxzP0n4r.jpeg");
+	    Image image5 = genImage("https://pbs.twimg.com/profile_images/454613053103828993/nxzP0n4r.jpeg");
+	    Image image6 = genImage("https://pbs.twimg.com/profile_images/454613053103828993/nxzP0n4r.jpeg");
+	    Image image7 = genImage("https://pbs.twimg.com/profile_images/454613053103828993/nxzP0n4r.jpeg");
+	    Image image8 = genImage("https://pbs.twimg.com/profile_images/454613053103828993/nxzP0n4r.jpeg");
+
+
+
+        title1.addStyleName("lessonTitle");
+        title2.addStyleName("lessonTitle");
+        title3.addStyleName("lessonTitle");
+        title4.addStyleName("lessonTitle");
+
+	    fp.add(image1);
+	    fp.add(image2);
+	    fp2.add(image3);
+	    fp2.add(image4);
+	    fp3.add(image5);
+	    fp3.add(image6);
+	    fp4.add(image7);
+	    fp4.add(image8);
 
         vp.add(title1);
 	    vp.add(fp);
 	    vp.add(title2);
+        vp.add(fp2);
 	    vp.add(title3);
+        vp.add(fp3);
 	    vp.add(title4);
-		
-		
+        vp.add(fp4);
+
+
 		add(vp, DockPanel.NORTH);
 
 
@@ -52,8 +77,9 @@ public class TaskSelectionPageBody extends PageBody{
     private Image genImage(String addr){
         Image img = new Image();
         img.setUrl(addr);
+        img.setPixelSize(100,100);
         return img;
-        
+
 
     }
 	@Override
